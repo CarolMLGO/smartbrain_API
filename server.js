1b5bb8e79441d6fe07972eb5dd2149e4dbdce1dc
Lei_tiger <leifu@oregonstate.edu>
2020-02-17 11:44:40 -0800

    minor changes

--- server.js
+++ server.js
@@ -1,40 +1,62 @@
-const express = require ('express');
+const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
-const knex = require('knex')
+const cors = require('cors'); //Uses additional HTTP headers to tell a browser to let a web application running at one origin have permission to access selected resources from a server at a different origin. 
+const knex = require('knex'); // SQL query builder for Postgres, MSSQL, MYSQL,MariaDB, SQLite3, Oracle, and Amazon Redshift. connect database to server
 
-const register = require('./controllers/register');
+const register = require('./controllers/register'); // import modules
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
+//run it on local host: connect database to server
+// const db = knex({
+//     client: 'pg',
+//     connection: {
+//         host: '127.0.0.1', //local host
+//         user: 'postgres',
+//         password: '198933jone',
+//         database: 'smart_brain'
+//     }
+// });
+
+// deploy on heroku: connect database to server
 const db = knex({
-  	client: 'pg',
-  	connection: {
-    connectionString : process.env.DATABASE_URL,
-    ssl:true
- 
-  }
+    client: 'pg',
+    connection: {
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
+    }
 });
+
+// how to access data from database
+// db.select('*').from('users').then(
+//     data => {
+//         console.log(data)
+//     });
+
 
 // const path = require('path'); //it's a core module, do not need to install
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/',(req,res)=>{
-	res.send('server is working')
+//test front end connection
+// app.get('/', (req, res) => {
+//     db.select('*').from('users').where({ email: "fuqionglei2014@gmail.com" }).then(
+//         data => {
+//             res.send(data)
+//         });
+//     // res.send('server is working')
+// })
+
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', register.handleRegister(db, bcrypt))
+app.get('/profile/:id', profile.handleProfile(db))
+app.put('/image', image.handleImage(db))
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
+
+const PORT = process.env.PORT;
+app.listen(PORT || 3000, () => {
+    console.log(`Server is listening on port 3000`);
 })
-
-app.post('/signin',signin.handleSignin(db,bcrypt))
-app.post('/register',register.handleRegister(db,bcrypt))
-app.get('/profile/:id',profile.handleProfile(db))
-app.put('/image',image.handleImage(db))
-app.post('/imageurl',(req,res)=>{image.handleApiCall(req,res)})
-
-app.listen(process.env.PORT || 3000,()=>{
-	console.log(`Server is listening on port ${process.env.PORT}`);
-})
-
-
